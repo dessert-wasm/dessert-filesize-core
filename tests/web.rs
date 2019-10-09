b056b291655a5a8c82e5d6bc0afa21bc93339f55
Nyquase <adrien.rouhete@epitech.eu>
2019-10-09 18:00:49 +0900

    More tests

--- tests/web.rs
+++ tests/web.rs
@@ -30,6 +30,22 @@ fn test(val: f64, res: &str, options: &serde_json::Value) {
 }
 
 #[wasm_bindgen_test]
+fn string_as_param() {
+    assert_eq!(
+        filesize(&JsValue::from_str("500"), &JsValue::UNDEFINED),
+        "500 B"
+    );
+}
+
+#[wasm_bindgen_test]
+fn base() {
+    let options = json!({
+        "base": 10,
+    });
+    test(5000.0, "5 kB", &options);
+}
+
+#[wasm_bindgen_test]
 fn spacer() {
     let options = json!({
         "spacer": "   ",
@@ -51,4 +67,61 @@ fn exponent() {
         "exponent": 2,
     });
     test(500_000_000.0, "476.84 MB", &options);
+}
+
+#[wasm_bindgen_test]
+fn fullform() {
+    let options = json!({
+        "fullform": true,
+    });
+    test(500.0, "500 bytes", &options);
+}
+
+#[wasm_bindgen_test]
+fn fullforms() {
+    let options = json!({
+        "fullform": true,
+        "fullforms": ["oui"],
+    });
+    test(12.0, "12 oui", &options);
+}
+
+#[wasm_bindgen_test]
+fn locale() {
+    let options = json!({
+        "locale": "de",
+    });
+    test(500_000.0, "488,28 KB", &options);
+}
+
+#[wasm_bindgen_test]
+fn round() {
+    let options = json!({
+        "round": 5,
+    });
+    test(50000.0, "48.82813 KB", &options);
+}
+
+#[wasm_bindgen_test]
+fn separator() {
+    let options = json!({
+        "separator": "oui",
+    });
+    test(50000.0, "48oui83 KB", &options);
+}
+
+#[wasm_bindgen_test]
+fn standard() {
+    let options = json!({
+        "standard": "iec",
+    });
+    test(5000.0, "4.88 KiB", &options);
+}
+
+#[wasm_bindgen_test]
+fn unix() {
+    let options = json!({
+        "unix": true,
+    });
+    test(500.0, "500", &options);
 }
